c5df4c01a21e4398b2a1618b85274fc49d3eab7a
佳伟 <s2265681@163.com>
2024-06-26 11:05:20 +0800

    feat: first

--- IOS_Study/ViewController.m
+++ IOS_Study/ViewController.m
@@ -7,15 +7,100 @@
 
 #import "ViewController.h"
 
+
+@interface TestView : UIView
+@end
+
+@implementation TestView
+
+-(instancetype)init{
+    self = [super init];
+    if(self){
+        
+    }
+    return self;
+}
+
+- (void)willMoveToSuperview:(nullable UIView *)newSuperview{
+    [super willMoveToSuperview:newSuperview];
+}
+- (void)didMoveToSuperview{
+    [super didMoveToSuperview];
+}
+
+- (void)willMoveToWindow:(nullable UIWindow *)newWindow{
+    [super willMoveToSuperview:newWindow];
+}
+
+- (void)didMoveToWindow{
+    [super didMoveToWindow];
+}
+@end
+
+
 @interface ViewController ()
 
 @end
 
 @implementation ViewController
 
+-(instancetype)init{
+    self = [super init];
+    if(self){
+        
+    }
+    return self;
+}
+
+- (void)viewWillAppear:(BOOL)animated{
+    [super viewWillAppear:animated];
+}
+- (void)viewIsAppearing:(BOOL)animated{
+    [super viewIsAppearing:animated];
+}
+- (void)viewDidAppear:(BOOL)animated{
+    [super viewDidAppear:animated];
+}
+- (void)viewWillDisappear:(BOOL)animated{
+    [super viewWillDisappear:animated];
+}
+
+- (void)viewDidDisappear:(BOOL)animated{
+    [super viewDidDisappear:animated];
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    self.view.backgroundColor = [UIColor whiteColor];
+    
     // Do any additional setup after loading the view.
+    // 设置一个 hello world
+//    [self.view addSubview:({
+//        UILabel *label = [[UILabel alloc] init];
+//        label.text = @"hello world";
+//        [label sizeToFit];
+//        label.center = CGPointMake(self.view.frame.size.width/2, self.view.frame.size.height/2);
+//        label;
+//    })];
+//    
+//    UIView *view2  = [[UIView alloc] init];
+//    view2.backgroundColor = [UIColor redColor];
+//    view2.frame = CGRectMake(100, 100, 100, 100);
+//    [self.view addSubview:view2];
+//    
+//    UIView *view  = [[UIView alloc] init];
+//    view.backgroundColor = [UIColor greenColor];
+//    view.frame = CGRectMake(110, 150, 100, 100);
+//    [self.view addSubview:view];
+//    
+    
+    
+        TestView *view  = [[TestView alloc] init];
+        view.backgroundColor = [UIColor greenColor];
+        view.frame = CGRectMake(110, 150, 100, 100);
+        [self.view addSubview:view];
+    
 }
 
 
